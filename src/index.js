8e0a6b73b3f0f0b5fb909d63eb532c2847d0be47
Andrew Weinstein <andrew@altvia.com>
2020-11-27 18:38:51 -0700

    set up socket.io

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
-
-const publicPath = path.join(__dirname, '../public');
+const socketio = require('socket.io');
 
 const app = express();
+const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
+const server = http.createServer(app);
+const ioServer = socketio(server);
+ioServer.on('connection', () => {
+  console.log('handling connection');
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Express listening on port ${port}`);
 });
